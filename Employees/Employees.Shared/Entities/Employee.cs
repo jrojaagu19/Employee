92b6f34b2fa6cf1735718fa8b2432c11e1de5121
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2025-09-14 18:23:58 -0500

    Create entities employees and responses

---
+++ Employees/Employees.Shared/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employees.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Apellido")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Activo")]
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Fecha de contratación")]
+    public DateTime HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [Range(1000000, double.MaxValue, ErrorMessage = "El salario debe ser mínimo $1,000,000.")]
+    public decimal Salary { get; set; }
+}
