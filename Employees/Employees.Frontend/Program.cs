791d2b680b403fcb331a96f0e784fc95f4ffd02e
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2025-10-12 17:44:13 -0500

    Creation repositories in the Frontend

--- Employees/Employees.Frontend/Program.cs
+++ Employees/Employees.Frontend/Program.cs
@@ -1,32 +1,25 @@
-using MudBlazor.Services;
 using Employees.Frontend.Components;
+using Employees.Frontend.Repositories;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add MudBlazor services
 builder.Services.AddMudServices();
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7200") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 app.Run();
