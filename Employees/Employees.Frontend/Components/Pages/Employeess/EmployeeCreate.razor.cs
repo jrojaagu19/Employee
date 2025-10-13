84631e617d777f788151677c37fce7e106c6bb6f
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2025-10-12 22:30:26 -0500

    Complete in Component the EmployeeCreate and employeeEdit

--- Employees/Employees.Frontend/Components/Pages/Employeess/EmployeeCreate.razor.cs
+++ Employees/Employees.Frontend/Components/Pages/Employeess/EmployeeCreate.razor.cs
@@ -1,5 +1,35 @@
+using Employees.Frontend.Repositories;
+using Employees.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
 namespace Employees.Frontend.Components.Pages.Employeess;
 
 public partial class EmployeeCreate
 {
+    private Employee employee = new();
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    private async Task CreateAsync()
+    {
+        var responseHttp = await Repository.PostAsync("api/employees", employee);
+
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        Return();
+        Snackbar.Add("Registro creado exitosamente.", Severity.Success);
+    }
+
+    private void Return()
+    {
+        NavigationManager.NavigateTo("/employees");
+    }
 }
