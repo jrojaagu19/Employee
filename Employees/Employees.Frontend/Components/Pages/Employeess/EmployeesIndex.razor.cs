219e5cf0ad14f53ed86cd080bfcc7485f5d7d981
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2025-10-12 21:10:38 -0500

    create Shared components  for filter and confirm  dialog

--- Employees/Employees.Frontend/Components/Pages/Employeess/EmployeesIndex.razor.cs
+++ Employees/Employees.Frontend/Components/Pages/Employeess/EmployeesIndex.razor.cs
@@ -2,19 +2,17 @@ using Employees.Frontend.Repositories;
 using Employees.Shared.Entities;
 using Microsoft.AspNetCore.Components;
 
-namespace Employees.Frontend.Components.Pages.Employeess
+namespace Employees.Frontend.Components.Pages.Employeess;
 
+public partial class EmployeesIndex
 {
-    public partial class EmployeesIndex
-    {
-        [Inject] private IRepository Repository { get; set; } = null!;
-        private List<Employee>? employees;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    private List<Employee>? employees;
 
-        protected override async Task OnInitializedAsync()
-        {
-            var httpResult = await Repository.GetAsync<List<Employee>>("api/employees");
-            Thread.Sleep(3000);
-            employees = httpResult.Response;
-        }
+    protected override async Task OnInitializedAsync()
+    {
+        var httpResult = await Repository.GetAsync<List<Employee>>("api/employees");
+        Thread.Sleep(3000);
+        employees = httpResult.Response;
     }
 }
