1f55054b7be1d4223a50a443812db3bdda0ffe45
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2025-11-29 03:34:49 -0500

    add Auth Changepassword, edituser; add helpers Ifilestorage

--- Employees/Employees.Backend/Controllers/AccountsController.cs
+++ Employees/Employees.Backend/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
-﻿using Employees.Backend.UnitsOfWork.Interfaces;
+﻿using Employees.Backend.Helpers;
+using Employees.Backend.UnitsOfWork.Interfaces;
 using Employees.Shared.DTOs;
 using Employees.Shared.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -12,22 +13,103 @@ using System.Text;
 namespace Employees.Backend.Controllers;
 
 [ApiController]
-[Route("/api/accounts")]
+[Route("api/[controller]")]
 public class AccountsController : ControllerBase
 {
     private readonly IUsersUnitOfWork _usersUnitOfWork;
     private readonly IConfiguration _configuration;
+    private readonly IFileStorage _fileStorage;
+    private readonly string _container;
 
-    public AccountsController(IUsersUnitOfWork usersUnitOfWork, IConfiguration configuration)
+    public AccountsController(IUsersUnitOfWork usersUnitOfWork, IConfiguration configuration, IFileStorage fileStorage)
     {
         _usersUnitOfWork = usersUnitOfWork;
         _configuration = configuration;
+        _fileStorage = fileStorage;
+        _container = "users";
+    }
+
+    [HttpPost("changePassword")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> ChangePasswordAsync(ChangePasswordDTO model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _usersUnitOfWork.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors.FirstOrDefault()!.Description);
+        }
+
+        return NoContent();
+    }
+
+    [HttpPut]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> PutAsync(User user)
+    {
+        try
+        {
+            var currentUser = await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!);
+            if (currentUser == null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrEmpty(user.Photo))
+            {
+                var photoUser = Convert.FromBase64String(user.Photo);
+                user.Photo = await _fileStorage.SaveFileAsync(photoUser, ".jpg", _container);
+            }
+
+            currentUser.Document = user.Document;
+            currentUser.FirstName = user.FirstName;
+            currentUser.LastName = user.LastName;
+            currentUser.Address = user.Address;
+            currentUser.PhoneNumber = user.PhoneNumber;
+            currentUser.Photo = !string.IsNullOrEmpty(user.Photo) && user.Photo != currentUser.Photo ? user.Photo : currentUser.Photo;
+            currentUser.CityId = user.CityId;
+
+            var result = await _usersUnitOfWork.UpdateUserAsync(currentUser);
+            if (result.Succeeded)
+            {
+                return Ok(BuildToken(currentUser));
+            }
+
+            return BadRequest(result.Errors.FirstOrDefault());
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!));
     }
 
     [HttpPost("CreateUser")]
     public async Task<IActionResult> CreateUser([FromBody] UserDTO model)
     {
         User user = model;
+        if (!string.IsNullOrEmpty(model.Photo))
+        {
+            var photoUser = Convert.FromBase64String(model.Photo);
+            model.Photo = await _fileStorage.SaveFileAsync(photoUser, ".jpg", _container);
+        }
+
         var result = await _usersUnitOfWork.AddUserAsync(user, model.Password);
         if (result.Succeeded)
         {
